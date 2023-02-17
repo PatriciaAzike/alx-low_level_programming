79a6e2ce9aa34bb0aa74920a12048b241b53d075
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-16 23:15:09 -0800

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int alph;
+        for (alph = 'a'; alph <= 'z'; alph++)
+        {
+                putchar(alph);
+	}
+	for (alph = 'A'; alph <= 'Z'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
