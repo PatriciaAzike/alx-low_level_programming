c6a1ab0951641e01fb79e0861e541e67cd244268
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-16 23:15:47 -0800

    print lowercase alphabets except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int alph;
+        for (alph = 'a'; alph<='z'; alph++)
+        {
+                if ((alph != 'e') && (alph != 'q'))
+		{
+			putchar(alph);
+		}
+        }
+        putchar('\n');
+        return (0);
+}
