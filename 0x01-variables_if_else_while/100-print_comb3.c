9904eb327d7a63d86328b644ac6455f583446a7d
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-17 02:31:46 -0800

    Print all possible combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,21 @@
  */
 int main(void)
 {
-        int i,j;
-        for (i = 0; i < 10; i++)
-        {
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
 		for (j = i + 1; j < 10; j++)
 		{
-			 putchar('0' + i);
-			 putchar('0' + j);
-			 if (i !=8 || j != 9)
-			 {
-				 putchar(',');
-				 putchar(' ');
-			 }
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i !=8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}	
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
