fb3967f97be9826e795ae0aac9388127508baf7b
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-16 23:56:08 -0800

    Heexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i = 0;
+        while (i < 16)
+        {
+		char hex;
+		if (i < 10)
+		{
+			hex = '0' + i;
+		}
+		else
+		{
+			hex = 'a' + (i - 10);
+		}
+                putchar(hex);
+		i++;
+        }
+        putchar('\n');
+        return (0);
+}
