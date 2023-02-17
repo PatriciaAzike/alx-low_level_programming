2535dcd66cbe695c2138eb53a91483275d483b0c
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-17 02:45:38 -0800

    Print all possible combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,9 +19,9 @@ int main(void)
 			for (k = j + 1; k < 10; k++)
 			{
 				putchar('0' + i);
-                         	putchar('0' + j);
-                         	putchar('0' + k);
-                         	if ((i != 7 || j != 8) || k != 9)
+				putchar('0' + j);
+				putchar('0' + k);
+				if ((i != 7 || j != 8) || k != 9)
                          	{
 					putchar(',');
 					putchar(' ');
