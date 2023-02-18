c569688e71ebdc2a371371e745973ecb94ee222b
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-17 18:06:49 -0800

    Doubble digits!

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,31 +10,30 @@
  */
 int main(void)
 {
-	int i, j, k, l;
-	
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			for (k = i; k < 10; k++)
-			{
-				for (l = j + 1; l < 10; l++)
-				{
-					putchar('0' + i);
-					putchar('0' + j);
-					putchar(' ');
-					putchar('0' + k);
-					putchar('0' + l);
-					if (!((i == 9 && j == 8) && (k == 9 && l == 9)))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				l = 0;
+	int i = 0, j;
 
-			}	   
+	while (i < 100)
+	{
+		j = i;
+
+		while (j < 100)
+		{
+			if (i != j)
+			{
+				putchar('0' + (i / 10));
+				putchar('0' + (i % 10));
+				putchar(' ');
+				putchar('0' + (j / 10));
+				putchar('0' + (j % 10));
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
 		}
+		i++;
 	}
 	putchar('\n');
 	return (0);
