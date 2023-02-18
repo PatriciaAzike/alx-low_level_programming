2004ab21e699f2879b77da77eec4d3127c9f003b
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-17 17:42:14 -0800

    DOuble digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,27 +10,30 @@
  */
 int main(void)
 {
-	int i = 0, j = 0;
+	int i, j, k, l;
 
-	while (i < 100)
+	for (i = 0; i < 10; i++)
 	{
-		j = i;
-
-		while (j < 100)
+		for (j = 0; j < 10; j++)
 		{
-			putchar('0' + (i / 10));
-			putchar('0' + (i % 10));
-			putchar(' ');
-			putchar('0' + (j / 10));
-			putchar('0' + (j % 10));
-			if (i != 99 || j != 99)
+			for (k = i; k < 10; k++)
 			{
-				putchar(',');
-				putchar(' ');
-			}
-			j++;
+		   		for (l = j + 1; l < 10; l++)
+				{
+			   		putchar('0' + i);
+			   		putchar('0' + j);
+			   		putchar(' ');
+			   		putchar('0' + k);
+			   		putchar('0' + l);
+					if (!((i == 9 && j == 8) && (k == 9 && l == 9)))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+			}	   
 		}
-		i++;
 	}
 	putchar('\n');
 	return (0);
