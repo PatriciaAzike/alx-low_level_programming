f79f7b0f8e96899c4124ec447e44f65a4d025e61
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-21 09:49:53 -0800

    Prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * print_sign - prints the sign of a number
+ * @n: integer
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
