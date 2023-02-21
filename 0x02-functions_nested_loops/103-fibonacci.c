f726efaab8ed701adc03bdc97b06892633f884d8
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-21 14:40:55 -0800

    Sum of even Fibonacci numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	long int x1 = 1, x2 = 2, x3 = 0, ev_fib = 0;
 
-	while (x3 <= 4000000)
+	while (x2 <= 4000000)
 	{
+		if (x2 % 2 == 0)
+		{
+			ev_fib += x2;
+		}
 		x3 = x1 + x2;
 		x1 = x2;
 		x2 = x3;
-		if (x3 % 2 == 0)
-		{
-			ev_fib += x3;
-		}
 	}
 	printf("%ld\n", ev_fib);
 	return (0);
