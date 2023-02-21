f7cd289a8ad2b769d13ca5ffae7b98b012bbe8cf
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-21 15:15:56 -0800

    First 98 fibonacci terms

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	double x1 = 1, x2 = 2, x3;
-	int i = 0;
+	int i = 2;
 
 	printf("%.0f, %.0f", x1, x2);
-	while (i < 96)
+	while (i < 98)
 	{
 		x3 = x1 + x2;
-		i++;
 		printf(", %.0f", x3);
 		x1 = x2;
 		x2 = x3;
+		i++;
 	}
 	printf("\n");
 	return (0);
