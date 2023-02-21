12004b64fa820e493b0814bcc94c90c9cee99f2a
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-21 14:11:22 -0800

    First 50 terms of Fibonacci sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - print the first Fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x1 = 1, x2 = 2, x3;
+	int i = 0;
+
+	printf("%d, %d", x1, x2);
+	while (i < x3)
+	{
+		x3 = x1 + x2;
+		i++;
+		printf(", %d", x3);
+		x1 = x2;
+		x2 = x3;
+	}
+	printf("\n");
+	return (0);
+}
