2c7e37ddfbc3436a155ae643c6b064e34b5cf7ab
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-21 09:52:07 -0800

    Computes the abs value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * _abs - computes the absolute value of an integer
+ * @x: integer
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int x)
+{
+	if  (x < 0)
+	 
+	{
+		return (-x);
+	}
+	else if (x == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (x);
+	}
+}
