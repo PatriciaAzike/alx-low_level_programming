67f4bf1b6718eec9c5da8c5f15ba93388ea5bd4a
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-21 11:10:51 -0800

    Jack Bauer's time schedule!

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /* betty style doc for function main goes there */
 /**
- * jack_bauer - function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - function that prints every minute Jack's day
  *
  * Return: Always 0 (Success)
  */
