dd7f88ffdf84591c1b2c455577b697b0869da4c4
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-21 15:00:11 -0800

    First 98 fibonacci terms

---
+++ 0x02-functions_nested_loops/104.fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - print the first 98 Fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x1 = 1, x2 = 2, x3;
+	int i = 0;
+
+	printf("%d, %d", x1, x2);
+	while(i < 96)
+	{
+		x3 = x1 + x2;
+		i++;
+		printf(", %d", x3);
+		x1 = x2;
+		x2 = x3;
+	}
+	printf("\n");
+	return (0);
+
+}
