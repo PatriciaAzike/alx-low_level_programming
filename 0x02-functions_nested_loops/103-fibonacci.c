b746ba413b91633d6b31a070866fd6148fb90df2
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-21 14:29:50 -0800

    Sum of even Fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - finds and prints the sum of even Fibonacci terms
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int x1 = 1, x2 = 2, x3 = 0, ev_fib = 0;
+
+	while (x3 <= 4000000)
+	{
+		x3 = x1 + x2;
+		if (x1 % 2 == 0)
+		{
+			ev_fib += x1;
+		}
+		x1 = x2;
+		x2 = x3;
+	}
+	printf("%ld\n", ev_fib);
+	return (0);
+}
