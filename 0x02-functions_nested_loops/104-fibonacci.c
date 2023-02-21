cb1b18211dd2863d0f906c6986ceb57e3f5c1e29
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-21 15:25:22 -0800

    First 98 fibonacci terms

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,26 @@
  */
 int main(void)
 {
-	double x1 = 1, x2 = 2, x3;
+	unsigned long x1 = 1, x2 = 2, x3;
 	int i = 2;
 
-	printf("%.0f, %.0f", x1, x2);
+	printf("%lu, %lu", x1, x2);
 	while (i < 98)
 	{
 		x3 = x1 + x2;
-		printf(", %.0f", x3);
+		i++;
+		if (x3 > 999999999)
+		{
+			unsigned long top = x3 / 1000000000;
+			unsigned long bot = x3 % 1000000000;
+			printf(", %lu%09lu", top, bot);
+		}
+		else
+		{
+			printf(", %lu", x3);
+		}
 		x1 = x2;
 		x2 = x3;
-		i++;
 	}
 	printf("\n");
 	return (0);
