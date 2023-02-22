c2e2b3bcd300dec09afda6023f8e3552d67c0c14
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-21 17:15:04 -0800

    First 98 terms in Fibonacci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print first 98 Fibonacci numbers 
  * Return: 0
@@ -6,30 +7,44 @@
 
 int main(void)
 {
-	unsigned long x1 = 1, x2 = 1, x3;
-	unsigned long h1, t1, h2, t2, bust;
-	int i;
+	unsigned long x1 = 1;
+	unsigned long x2 = 1;
+	unsigned long x3 = 0;
+	long t1, t2, h1, h2, x3_h, x3_t;
+	int i = 2, bust;
+
 	printf("1");
-	for (i = 2; i < 98; i++)
+
+	while (i < 93)
 	{
 		x3 = x1 + x2;
-		if (x3 > 999999999)
-		{
-			h1 = x2 / 1000000000;
-			t1 = x2 % 1000000000;
-			h2 = x3 / 1000000000;
-			t2 = x3 % 1000000000;
-			bust = (t1 + t2) / 1000000000;
-			printf(", %lu%09lu", h1 + h2 + bust, (t1 + t2) % 1000000000);
-		}
-		else
-		{
-			printf(", %lu", x3);
-		}
+		i++;
+		printf(", %lu", x3);
 		x1 = x2;
 		x2 = x3;
 	}
+
+	h1 = x1 / 1000000000;
+	t1 = x1 % 1000000000;
+	h2 = x2 / 1000000000;
+	t2 = x2 % 1000000000;
+
+	while (i < 99)
+	{
+		bust = (t1 + t2) / 1000000000;
+		x3_t = (t1 + t2) - (1000000000 * bust);
+		x3_h = (h1 + h2) + bust;
+		i++;
+		printf(", %lu%09lu", x3_h, x3_t);
+
+		h1 = h2;
+		t1 = t2;
+		h2 = x3_h;
+		t2 = x3_t;
+	}
+
+
 	printf("\n");
+
 	return (0);
 }
-
