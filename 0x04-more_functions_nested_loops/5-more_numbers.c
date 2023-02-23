671354e7eadaeffdf836980f656995bca14d8a61
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-23 02:20:43 -0800

    Prints numbers x 10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * more_numbers - prints numbers 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int num;
+
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar('0' + num / 10); 
+			}
+			_putchar('0' + num % 10);
+		}
+		_putchar('\n');
+	}
+}
