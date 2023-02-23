c71193939a4b20362e00614a47220c630bcd7fad
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-02-23 03:02:03 -0800

    Prints diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line n times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		while (i < n)
+		{
+			int j = 0;
+
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar(92);
+			_putchar('\n');
+			i++;
+		}
+	}
+}
