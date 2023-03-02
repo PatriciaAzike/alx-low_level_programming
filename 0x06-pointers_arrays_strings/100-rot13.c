3a04f174d43fc26efeaab9ab0f9b6d9a27fe00da
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-02 03:01:48 -0800

    Rotate encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encode string using rot13
+ * @s: string
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+       	char *str = s;
+ 	char c;
+
+ 	while (*s)
+  	{
+		if ((*s >= 'a' && *s <= 'm') || (*s >= 'A' && *s <= 'M'))
+		{
+			c = *s + 13;
+		}
+		else if ((*s >= 'n' && *s <= 'z') || (*s >= 'N' && *s <= 'Z'))
+		{
+			c = *s - 13;
+		}
+		else
+		{
+			c = *s;
+		}
+		*s++ = c;
+	}
+  return (str);
+}
