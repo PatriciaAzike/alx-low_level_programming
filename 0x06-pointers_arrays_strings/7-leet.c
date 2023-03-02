2d8fd5508840eabb7d637a852e7c764d68c2b0b1
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-02 01:28:15 -0800

    encodes string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,36 +1,31 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * leet - a function that encodes a string into 1337.
- * @str: string
- *
- * Return: Always 0.
+ * leet - function that encodes string into 1337
+ * @s: string
+ * Return: string
  */
-char *leet(char *str)
-{
-	int i = 0, j = strlen(str);
-	char leet_map[128] = {0};
-	char *leet_str = str;
 
-	/* fill leet_map with the character mappings */
-	leet_map['a'] = leet_map['A'] = '4';
-	leet_map['e'] = leet_map['E'] = '3';
-	leet_map['o'] = leet_map['O'] = '0';
-	leet_map['t'] = leet_map['T'] = '7';
-	leet_map['l'] = leet_map['L'] = '1';
-	
-	while (i < j)
+char *leet(char *s)
+{
+	int i = 0;
+	int x[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int y[11] = {'4','4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	while (s[i] != '\0')
 	{
-		char c = str[i];
-		char leet_c = leet_map[(int)c];
-		
-		if (leet_c != 0)
+		int j = 0;
+
+		while (j < 11)
 		{
-			leet_str[i] = leet_c;
+			if (s[i] == x[j])
+			{
+				s[i] = y[j];
+				break;
+			}
+			j++;
 		}
 		i++;
 	}
-    return (leet_str);
+	return (s);
 }
