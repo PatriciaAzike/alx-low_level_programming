aea8e73a9ac3dff10c867e2445377492e719869f
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-02 11:28:08 -0800

    Capitalize words in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * cap_string - capitalizes all words of a string
- * @ch: character
- *
- * Return: Always 0.
+ * cap_string - capitalize first letter of each word in a string
+ * @s: string 
+ * Return: string
  */
+
 char *cap_string(char *s)
 {
 	int i = 0;
-	char *sep = " \t\n,;.!?\"(){}";
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
 
 	while (s[i] != '\0')
 	{
-		if (i == 0 || strchr(sep, s[i-1]))
-		{
-			s[i] = (s[i] >= 'a' && s[i] <= 'z') ?  (s[i] - 'a' + 'A') : s[i];
-		}
-		/*else if (s[i] >= 'A' && s[i] <= 'Z')
-		{
-			s[i] -= 'A' + 'a';
-		}*/
-		/*else if (s[i] == ' ')
-		{
-			j--;
-			i++;
-			continue;
-		}
-		i++;*/
+		if ((s[i] >= 'a' && s[i] <= 'z')
+		    && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			s[i] = s[i] - 'a' + 'A';
 		i++;
 	}
-	s[i] = '\0';
+
 	return (s);
 }
