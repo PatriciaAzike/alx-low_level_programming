fbeb510b7f05a291f5eb774b42e98373cd3ad035
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-02 18:55:05 -0800

    Add two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,61 +1,51 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer to store output
+ * @n1: first num
+ * @n2: second num
+ * @r: buffer to store the result.
  * @size_r: buffer size:
- * Return: r
+ * Return: r.
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0;
-	int res, curr1, curr2, res_index = size_r - 1;
-	
-	while (*(n1 + len1) != '\0')
-	{
-		len1++;
-	}
-	while (*(n2 + len2) != '\0')
-	{
-		len2++;
-	}
-	if (len1 >= len2)
-	{
-		res_index = len1;
-	}
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
 	else
-	{
-		res_index = len2;
-	}
-	if (size_r <= res_index + 1)
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
-	r[res_index + 1] = '\0';
-	len1--, len2--, size_r--;
-	curr1 = *(n1 + len1) - 48;
-	curr2 = *(n2 + len2) - 48;
-	while (res_index >= 0)
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		res = curr1 + curr2 + carry;
-		if (res >= 10)
-			carry = res / 10;
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
 		else
-			carry = 0;
-		if (res > 0)
-			*(r + res_index) = (res % 10) + 48;
+			add = 0;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
 		else
-			*(r + res_index) = '0';
-		if (len1 > 0)
-			len1--, curr1 = *(n1 + len1) - 48;
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
 		else
-			curr1 = 0;
-		if (len2 > 0)
-			len2--, curr2 = *(n2 + len2) - 48;
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
 		else
-			curr2 = 0;
-		res_index--, size_r--;
+			dr2 = 0;
+		bg--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
