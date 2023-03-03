88ae1f89fbd64efedb5109979817d918ab1d2d2a
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-02 17:08:25 -0800

    Print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -20,15 +20,15 @@ void print_buffer(char *b, int size)
     
 	for (i = 0; i < size; i += 10)
 	{
-		printf("%08x: ", i);
+		printf("%.8x: ", i);
 		for (j = i; j < i + 10; j += 2)
 		{
 			if (j < size)
 			{
-				printf("%02x", b[j] & 0xff);
+				printf("%.2x", b[j] & 0xff);
 				if (j + 1 < size)
 				{
-					printf("%02x", b[j + 1] & 0xff);
+					printf("%.2x", b[j + 1] & 0xff);
 				}
 				else
 				{
