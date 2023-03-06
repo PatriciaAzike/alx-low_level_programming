581c115f7731b6281d71f9cdf0090b5ddeb74995
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-06 12:29:21 -0800

    Locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,6 @@
  * _strchr - locates a character in a string
  * @s: string
  * @c: character
- *
  * Return: pointer to the first occurence of c in s or NULL
  */
 char *_strchr(char *s, char c)
@@ -18,7 +17,7 @@ char *_strchr(char *s, char c)
 	}
 	if (c == '\0')
 	{
-		return s;
+		return (s);
 	}
 	return ('\0');
 }
