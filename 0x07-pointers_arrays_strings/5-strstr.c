23649515342f927e4e35a4fca0531e36ea2f7d29
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-06 14:26:06 -0800

    Locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,23 @@
 #include "main.h"
-
+#define NULL 0
 /**
- * _strstr - locates substring
- * @haystack: char
- * @needle: char to compare
- * Return: pointer to the beginning of located substring
-*/
-char *_strstr(char *haystack, char *needle)
+ * _strstr - locates a substring
+ * @haystack: first character
+ * @needle: second character
+ * Return: pointer to the beginning of the located substring
+ */
+ char *_strstr(char *haystack, char *needle)
 {
-	char *hay
-	char *need
+	int i, j;
 
-	while (*haystack != '\0')
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		hay = haystack;
-		need = needle;
-
-		while (*haystack != '\0' && *need != '\0' && *haystack == *need)
-		{
-			haystack++;
-			need++;
-		}
-		if (!*need)
-			return (hay);
-		haystack = hay + 1;
+		for (j = 0; needle[j] != '\0' && haystack[i+j] == needle[j]; j++)
+			;
+		if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
 	}
-	return (0);
+	return (NULL);
 }
