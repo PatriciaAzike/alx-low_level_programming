b2c0de82257c0b373c496fe661762a58808c0426
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-06 13:25:21 -0800

    searh a string for any set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,15 +4,15 @@
  * _strpbrk - searches a string for any of a set of bytes
  * @s: string
  * @accept: character
- * Return: Returns a pointer to the byte in s that matches a byte in accept
+ * Return: Returns a pointer to the byte in s that matches accept
  */
  char *_strpbrk(char *s, char *accept)
 {
 	char *p = accept;
 
-	while (*s != '\0')
+	while (*s)
 	{
-		while (*p != 0)
+		while (*p)
 		{
 			if (*s == *p)
 			{
