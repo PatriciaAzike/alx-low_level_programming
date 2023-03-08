b9edd30e14f6799ade384bbf64f54c625f778bd3
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-08 02:28:56 -0800

    checks f a number is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: integer
+ * Return: 1 if prime, 0 otherwise
+ */
+int helper(int , int);
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (helper(n, 2));
+}
+/**
+ * helper - helper function
+ * @x: integer
+ * @i: divisor
+ * Return: 1 if prime, 0 otherwise
+ */i
+int helper(int x, int i)
+
+{
+	if (x == 2)
+	{
+		return (1);
+	}
+	else 
+	{
+		if (x % i == 0)
+		{
+			return (0);
+		}
+		else if (i * i > x)
+		{
+			return (1);
+		}
+	}
+	return helper(x, i + 1);
+}
