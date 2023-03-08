455f6df2e004ab63a712906291608b4fdb4924a0
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-07 23:49:10 -0800

    Print a string followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: character
+ * Return: No return
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
