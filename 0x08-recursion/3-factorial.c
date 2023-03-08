2cd83fdb3cdc8d1ab9a72ee62f1147f3925577c2
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-08 00:30:51 -0800

    prints factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - gives the factorial of a number
+ * @n: integer
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
