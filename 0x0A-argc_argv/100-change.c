cdaa6ef52e95e8392eaf3b32680a0942403d1ad2
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-10 05:13:32 -0800

    Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,26 +15,21 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
-		printf("0\n");
-	else
-	{	
-		while (cents > 0)
-		{
-			if (cents >= 25)
-				cents -= 25;
-			else if (cents >= 10)
-				cents -= 10;
-			else if (cents >= 5)
-				cents -= 5;
-			else if (cents >= 2)
-				cents -= 2;
-			else if (cents >= 1)
-				cents -= 1;
-			change += 1;
-		}
-		printf("%d\n", change);
+	cents = atoi(argv[1]);	
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		change += 1;
 	}
+	printf("%d\n", change);
 	return (0);
 }
