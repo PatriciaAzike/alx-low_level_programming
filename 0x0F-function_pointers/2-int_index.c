75ad8876e62607eb4ef48e8f6bdeba7eb1d276a8
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-22 13:53:02 -0700

    COrrect index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,12 +10,13 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
 	if (array == NULL || cmp == NULL)
 		return (-1);
 	if (size <= 0 )
 		return (-1);
+	i = 0;
 	while (i < size)
 	{
 		if (cmp(array[i]) != 0)
