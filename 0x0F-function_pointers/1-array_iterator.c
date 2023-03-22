6f1875a89b01b57123bad60f847fe64366dfdc78
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-22 13:32:12 -0700

    Edited the descriptior for function name

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #include "function_pointers.h"
 /**
-*print_name - a function that executes a function
+*array_iterator - a function that executes a function
 *given as a parameter on each element of an array
 *@array: pointer variable of type int
 *@size: size of the array
@@ -10,18 +10,17 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (array == NULL || action == NULL)
 		return;
 	if (size > 0)
 	{
+		i = 0;
 		while (i < size)
 		{
 			action(array[i]);
 			i++;
 		}
 	}
-	else
-		return;
 }
