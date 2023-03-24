96fd83b8fa443aa6cfef9a622f93b1e3de38161f
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-23 17:04:51 -0700

    Sums all the parameters of a function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 #include "variadic_functions.h"
 /**
  * sum_them_all -  sums all parameters of the function
- * @n: number of elements to be added
+ * @n: number of parameters to be summed
  * Return: Returns the sum
  */
 int sum_them_all(const unsigned int n, ...)
@@ -14,7 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	while(i < n)
+	while (i < n)
 	{
 		sum += va_arg(arr, int);
 		i++;
