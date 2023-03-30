be55637046de661e1ee424db6a0105cc6e20f5b0
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-30 16:00:02 -0700

    Prints all the elements in a singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 
 /**
  * print_list - prints all the elements of a list_t list
- * @d: ponnter to head of list
+ * @h: ponnter to head of list
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
