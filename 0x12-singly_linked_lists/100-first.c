69bcbafa12322fab5c264246715b2a0eaca8dd76
PatriciaAzike <patricia.azike@aims.ac.rw>
2023-03-30 17:30:03 -0700

    Function that prints message

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_text - function that prints message
+ * Return: no return
+ */
+
+void __attribute__ ((constructor)) print_text()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
